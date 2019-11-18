0683cc130c4d2cd90c1ad9ba87b9df9db28e699c
Brian Bartholomew <me@bcbrian.com>
2019-11-18 11:16:26 -0600

    clean up and fixed demo bug

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function timer() {
     time = time - 1;
 
     let hours = Math.floor(time / 60 / 60);
-    let minutes = Math.floor(time / 60);
+    let minutes = Math.floor(time / 60) % 60;
     let seconds = time % 60;
 
     if (hours < 10) {
