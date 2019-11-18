0e9029ea65bac861565189c5422de95f8c76ffc0
Brian Bartholomew <me@bcbrian.com>
2019-11-18 12:02:21 -0600

    the time partially works

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function timer() {
-  const timerEl = document.getElementById("clock");
+  const defaultTime = 300;
+  const clockEl = document.getElementById("clock");
+  const userEl = document.getElementById("user-time-in-seconds");
+  let savedValue = localStorage.getItem("time");
+  let time = 0;
+  if (savedValue) {
+    time = savedValue;
+  } else {
+    time = defaultTime;
+  }
+  let myInterval = null;
 
   document.getElementById("start-button").addEventListener("click", function() {
-    console.log("star wars clicked");
+    const userInput = parseInt(userEl.value, 10);
+    time = userInput;
+    startTimer();
+    console.log("star wars clicked", userEl.value);
   });
 
   document.getElementById("minus-button").addEventListener("click", function() {
@@ -13,30 +26,36 @@ function timer() {
     console.log("plus was clicked");
   });
 
-  const defaultTime = 300;
-  let time = defaultTime;
-  let myInterval = setInterval(function() {
-    time = time - 1;
-
-    let hours = Math.floor(time / 60 / 60);
-    let minutes = Math.floor(time / 60) % 60;
-    let seconds = time % 60;
-
-    if (hours < 10) {
-      hours = "0" + hours;
-    }
-    if (minutes < 10) {
-      minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-
-    console.log(hours + ":" + minutes + ":" + seconds); // 00:00:00
-
-    if (time <= 0) {
+  function startTimer() {
+    if (myInterval) {
       clearInterval(myInterval);
+      myInterval = null;
     }
-  }, 1000);
+    myInterval = setInterval(function() {
+      time = time - 1;
+      localStorage.setItem("time", time);
+
+      let hours = Math.floor(time / 60 / 60);
+      let minutes = Math.floor(time / 60) % 60;
+      let seconds = time % 60;
+
+      if (hours < 10) {
+        hours = "0" + hours;
+      }
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      clockEl.innerHTML = hours + ":" + minutes + ":" + seconds;
+
+      if (time <= 0) {
+        clearInterval(myInterval);
+      }
+    }, 1000);
+  }
+  startTimer();
 }
 timer();
